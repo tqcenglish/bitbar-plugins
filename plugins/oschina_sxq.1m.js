3d4dc22088edfca5410f772f9c5e15230db9df93
tqcenglish <tqcenglish@gmail.com>
2017-06-30 09:18:50 +0800

    评论动弹时不能像新闻那样获取标题

--- plugins/oschina_sxq.1m.js
+++ plugins/oschina_sxq.1m.js
@@ -40,9 +40,14 @@ let getData = async() => {
         display[0].text = text['0'].innerHTML;
       }
 
-      let news = $(html).find(' #main-anchor > div.panels-body > div > div:nth-child(1) > div.flex-grow.tweet-body > div.tweet-dedails > div.reply-title > a')
-      if (news && news['0']) {
+      let news = $(html).find('#main-anchor > div.panels-body > div > div:nth-child(1) > div.flex-grow.tweet-body > div.tweet-dedails > div.reply-title > a')
+      if (news && news['0'] && news['0'].innerHTML.trim()) {
         display[2].text = news['0'].innerHTML.trim();
+      }else{
+        let dongtan = $(html).find('#main-anchor > div.panels-body > div > div:nth-child(1) > div.flex-grow.tweet-body > div.tweet-dedails > div.post > div.reply-quote > div > div')
+        if(dongtan && dongtan['0']){
+          display[2].txt = dongtan['0'].innerHTML.trim() || 'just a error dongtan';
+        }
       } 
 
       let url = $(html).find('#main-anchor > div.panels-body > div > div:nth-child(1) > div.flex-grow.tweet-body > div.relation-data > a')
